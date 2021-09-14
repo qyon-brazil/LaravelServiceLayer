86bef0696f8cc627f2b3ce320a76dee2330b9356
Gustavo Santarosa <gustavo.santarosa@qyon.com>
2021-09-14 09:58:43 -0300

    adicionado include, e a tratativa do dto com paginate
    organizado de acordo com as psr

--- src/ReturnPrepare.php
+++ src/ReturnPrepare.php
@@ -8,49 +8,63 @@ use Illuminate\Support\ServiceProvider;
  * ReturnPrepare Serve para responder requisições no Controller
  * Nota: baseada na Classe DataPrepare (Utilizada na API do ERP) 
  *
- * @author Diego Silva <diego@qyon.com>
  */
 class ReturnPrepare extends ServiceProvider
 {
     private static $_forbidenNames = ['code', 'msg', 'data', 'success'];
 
-    public static function getMessageDTO(DataTransferObject $dto,$http_code,$params=[])
-    {        
-        return self::getMessage($dto->getSuccess(), $dto->getMessage(),$http_code,$params,[$dto->getData()]);
+    public static function getMessageDTO(DataTransferObject $dto, $http_code)
+    {
+        return self::getMessage($dto->getSuccess(), $dto->getInclude(), $dto->getIndex(), $dto->getMessage(), $http_code, null, [$dto->getData()]);
     }
 
-    private static function getMessage($success, $message, $code, $params=[], $data=[]) {
-        
+    private static function getMessage($success, $include = [], $index = false, $message, $code, $params = [], $data = [])
+    {
+
+
         $retArr = array(
             "success" => $success,
-            "code" => $code,
-            "msg"  => $message,
+            "code"    => $code,
+            "msg"     => $message,
+            "include" => $include,
         );
 
-        if(isset($data[0]) && count($data) > 0){
-            
+        if (isset($data[0]) && count($data) > 1) {
+
             $retArr = array_merge($retArr, [
-                "totalindata"=> count($data),
+                "totalindata" => count($data),
             ]);
         }
 
-        if(is_array($data) && count($data) > 0){
+        if (is_array($data) && count($data) > 0) {
             $retArr['data'] = $data;
         }
 
-        if(is_array($params)){
-            foreach($params as $name => $value){
-                if(!in_array($value, self::$_forbidenNames)){
+        if (is_array($params)) {
+            foreach ($params as $name => $value) {
+                if (!in_array($value, self::$_forbidenNames)) {
                     $retArr[] = $value;
                 }
             }
+        }
+
+        if($index){
+            $retArr = array(
+                "success" => $success,
+                "code"    => $code,
+                "msg"     => $message,
+                "include" => $include,
+            );
+
+            $retArr = array_merge($retArr, $data[0]);
         }
         // ========================================
 
         return $retArr;
     }
 
-    public static function successMessage($message, $code, $params=[], $data=[]) {
+    public static function successMessage($message, $code, $params = [], $data = [])
+    {
         return self::getMessage(
             true,
             $message,
@@ -60,7 +74,8 @@ class ReturnPrepare extends ServiceProvider
         );
     }
 
-    public static function errorMessage($message, $code, $params=[], $data=[]) {
+    public static function errorMessage($message, $code, $params = [], $data = [])
+    {
         return self::getMessage(
             false,
             $message,
@@ -83,5 +98,5 @@ class ReturnPrepare extends ServiceProvider
         }
 
         return (object) $retArr;
-    }    
+    }
 }
